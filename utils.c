4027cd5ec82f00011d30fbba0370674b9ec96edc
Gianmarco D'Auria <gd-auria@e3r10p2.42roma.it>
2025-01-23 13:53:14 +0100

    aggiunto fuzioni lbft a utils.c ma ho eliminato per errore split, rimetterlo assieme a strlen

--- utils.c
+++ utils.c
@@ -1,6 +1,6 @@
 #include "struct.h"
 
-void    init(t_cmd *cmd)
+void	init(t_cmd *cmd)
 {
 	cmd->command = NULL;
 	cmd->args = malloc(sizeof(char *) * TOKEN_MAX);
@@ -9,4 +9,46 @@ void    init(t_cmd *cmd)
 	cmd->flag = 0;
 	if (!cmd)
 		return ;
+}
+
+int	ft_atoi(const char *str)
+{
+	int	result;
+	int	sign;
+
+	sign = 1;
+	result = 0;
+	while ((*str >= '\t' && *str <= '\r') || *str == ' ')
+		++str;
+	if (*str == '+' || *str == '-')
+	{
+		if (*(str++) == '-')
+			sign *= -1;
+	}
+	while (ft_isdigit(*str))
+	{
+		result = result * 10;
+		result += (*str++ - '0');
+	}
+	return (sign * result);
+}
+
+char	*ft_strdup(const char	*src)
+{
+	char	*copy;
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	len = ft_strlen(src);
+	copy = (char *)malloc(sizeof(char) * (len + 1));
+	if (copy == NULL)
+		return (NULL);
+	while (src[i] != '\0')
+	{
+		copy[i] = src[i];
+		i++;
+	}
+	copy[i] = '\0';
+	return (copy);
 }

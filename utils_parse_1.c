0d32cdd2749aa1fd8d4cb05a90a80afbbf20b86d
Christian Calabro <ccalabro@e3r10p2.42roma.it>
2025-01-30 15:39:42 +0100

    norminetteto(manca il main)

--- utils_parse_1.c
+++ utils_parse_1.c
@@ -6,7 +6,7 @@
 /*   By: ccalabro <ccalabro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:08:44 by ccalabro          #+#    #+#             */
-/*   Updated: 2025/01/30 15:32:45 by ccalabro         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:38:47 by ccalabro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,6 +28,7 @@ void	ft_input_redirect_parse(t_cmd *cmd, char **tokens, int i)
 			cmd->input = strdup(tokens[i]);
 	}
 }
+
 void	ft_output_redirect_parse(t_cmd *cmd, char **tokens, int i)
 {
 	if (*(tokens[i] + 1))
--- utils_tokenize_2.c
+++ utils_tokenize_2.c
@@ -6,7 +6,7 @@
 /*   By: ccalabro <ccalabro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 14:29:52 by ccalabro          #+#    #+#             */
-/*   Updated: 2025/01/30 14:54:52 by ccalabro         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:38:34 by ccalabro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,6 +29,7 @@ void	ft_output_redirect(t_tokenize *t, char *input)
 	t->token_count++;
 	printf("genera token operatore\n");
 }
+
 void	ft_input_redirect(t_tokenize *t, char *input)
 {
 	t->i = t->i + 1;

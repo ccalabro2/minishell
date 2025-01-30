ec799470d786b43cfa8df0ccc8b8f4eeac3ee538
Christian Calabro <ccalabro@e3r10p2.42roma.it>
2025-01-30 15:37:48 +0100

    corretto i nomi che avevamo invertito per sbaglio

--- parse.c
+++ parse.c
@@ -6,13 +6,12 @@
 /*   By: ccalabro <ccalabro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/23 18:36:40 by ccalabro          #+#    #+#             */
-/*   Updated: 2025/01/30 15:24:43 by ccalabro         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:35:46 by ccalabro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 /*TODO:
 -gestire anche le pipe
--accorciare la funzione dividendola in: (es)ft_operator per i primi 3 if e ft_cdm_args
 */
 #include "struct.h"
 
@@ -29,9 +28,9 @@ t_cmd	parse(char **tokens)
 	{
 		printf("(siamo in parse)TOKEN: %s\n", tokens[i]);
 		if (*tokens[i] == '<')
-			ft_output_redirect_parse(&cmd, tokens, i);
-		else if (*tokens[i] == '>')
 			ft_input_redirect_parse(&cmd, tokens, i);
+		else if (*tokens[i] == '>')
+			ft_output_redirect_parse(&cmd, tokens, i);
 		else if (!cmd.command)
 			cmd.command = strdup(tokens[i]);
 		else
--- utils_parse_1.c
+++ utils_parse_1.c
@@ -6,13 +6,13 @@
 /*   By: ccalabro <ccalabro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/30 15:08:44 by ccalabro          #+#    #+#             */
-/*   Updated: 2025/01/30 15:21:29 by ccalabro         ###   ########.fr       */
+/*   Updated: 2025/01/30 15:32:45 by ccalabro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "struct.h"
 
-void	ft_output_redirect_parse(t_cmd *cmd, char **tokens, int i)
+void	ft_input_redirect_parse(t_cmd *cmd, char **tokens, int i)
 {
 	if (*(tokens[i] + 1))
 	{
@@ -28,7 +28,7 @@ void	ft_output_redirect_parse(t_cmd *cmd, char **tokens, int i)
 			cmd->input = strdup(tokens[i]);
 	}
 }
-void	ft_input_redirect_parse(t_cmd *cmd, char **tokens, int i)
+void	ft_output_redirect_parse(t_cmd *cmd, char **tokens, int i)
 {
 	if (*(tokens[i] + 1))
 	{
